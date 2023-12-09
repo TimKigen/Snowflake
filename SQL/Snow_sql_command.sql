9faf9238160478be9327411819f0c6f07dcbb827
TimKigen <timothykipkorir3@gmail.com>
2023-12-10 00:25:56 +0300

    Initial commit

---
+++ SQL/Snow_sql_command.sql
@@ -0,0 +1,9 @@
+snowsql -f Dev_main.sql -a
+organisation.east-us-2.privatelink -u timkigen1@gmail.com --authenticator externalbrowser
+
+
+ --Use CMD in desktop to deploy --snowsql should be installed
+C:\Users\Soko\Desktop\Tim_github_repo\Snowflake\SQL
+
+ C:\Users\Soko\Desktop\Tim_github_repo\Snowflake\SQL>snowsql -f Dev_main.sql -a
+organisation.east-us-2.privatelink -u timkigen1@gmail.com --authenticator externalbrowser
